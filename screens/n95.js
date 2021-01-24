dad9d46569a3c471119309e7f7ceb6d6fea984ad
AnaPatricia <52417292+apolvm@users.noreply.github.com>
2021-01-24 15:49:29 -0500

    Add files via upload

--- screens/n95.js
+++ screens/n95.js
@@ -1,5 +1,6 @@
+/*Author: Brendon Wright*/
 import React from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
+import {StyleSheet, View, Text, Button, Image} from 'react-native';
 import {globalStyles} from '../styles/global'
 
 export default function N95({ navigation }){
@@ -8,9 +9,16 @@ export default function N95({ navigation }){
     }
     return(
         <View style={ globalStyles.container}>
-            <Text>N95 screen</Text>
+            <Image source={require('../assets/n95.png')} />
+            <Text>Make sure to remove your mask by holding the edge of the straps to take it off.As you're removing the mask from your face, make sure you're not touching the inside part of the mask as the mask may become contaminated.</Text>
+            <StatusBar style="auto" />
+                <View style={globalStyles.rating}>
+                <Text>These masks are reusuable, so be sure to place the mask in a breathable container,zip-lock bag, or a plastic bag.</Text>
+                <Text>If you do not wish to use the mask, place it in a zip-lock or plastic bag and dispose of the bag in a garbage can or biomedical waste disposal unit.</Text>
+                    </View>
             <Button title= 'Back' onPress={pressHandler}/>
 
         </View>
     )
 }
+
